f7fa272c0f7458a2e7302b08ea40533f659ad084
BigBoss <bigboss@sendnodes.io>
2022-07-19 21:06:31 -0500

    wip on tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./ui/**/*.{html,ts,tsx}"],
+  content: ["./ui/pages/**/*.{html,ts,tsx}", "./ui/components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        serif: ["Merriweather", "serif"],
+      },
+      colors: {
+        capri: "#33b8ff",
+        aqua: "#33ffff",
+      },
+    },
   },
   plugins: [],
 }
