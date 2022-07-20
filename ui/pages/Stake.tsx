b6f360b9ecf71fb162da4cc6ad56b6035ddaa385
BigBoss <bigboss@sendnodes.io>
2022-07-19 21:06:31 -0500

    wip on tailwind theme

--- ui/pages/Stake.tsx
+++ ui/pages/Stake.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Stake = () => {
   return (
     <div>
-      <h1 className="text-6xl">Stake</h1>
+      <h1 className="text-6xl text-aqua">Stake</h1>
     </div>
   )
 }
