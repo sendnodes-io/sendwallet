bd46a9dcaf300be50460d6058743452a01751198
BigBoss <bigboss@sendnodes.io>
2022-08-08 22:34:08 -0500

    layout updates

--- ui/components/Stake/Stat/StatTotalUpcomingRewards.tsx
+++ ui/components/Stake/Stat/StatTotalUpcomingRewards.tsx
@@ -30,9 +30,7 @@ export default function StatTotalUpcomingRewards({
         <div className="relative grow h-full">
           <div className="flex flex-col grow items-center justify-center h-full">
             <div className="text-4xl xl:text-5xl font-semibold text-white">
-              {isError ? (
-                (isError as any).toString()
-              ) : isLoading ? (
+              {isLoading ? (
                 <SharedLoadingSpinner />
               ) : (
                 formatTokenAmount(amount, 3, 1)
