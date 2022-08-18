c57568bce2de23e1dbcd82fc9af535e1a56636b2
BigBoss <bigboss@sendnodes.io>
2022-08-18 18:07:04 -0500

    fix firefox background page startup

--- src/background.ts
+++ src/background.ts
@@ -1,21 +1,20 @@
-import logger from "@sendnodes/pokt-wallet-background/lib/logger"
-import browser from "webextension-polyfill"
-const PROCESS_ID = ((globalThis as any).PROCESS_ID =
-  "background-" + Math.random() + "." + new Date().getTime())
-
-const serviceWorker = self as unknown as ServiceWorkerGlobalScope
-
-serviceWorker.addEventListener("install", (event: ExtendableEvent) => {
-  event.waitUntil(serviceWorker.skipWaiting())
-})
-
-browser.runtime.onMessage.addListener((message, sender) => {
-  if (message.type === "HEARTBEAT") {
-    logger.debug("background received heartbeat message", message, sender)
-    return Promise.resolve({ processId: PROCESS_ID })
-  }
-})
 ;(async () => {
+  const browser = await import("webextension-polyfill")
+  const PROCESS_ID = ((globalThis as any).PROCESS_ID =
+    "background-" + Math.random() + "." + new Date().getTime())
+
+  const serviceWorker = self as unknown as ServiceWorkerGlobalScope
+
+  serviceWorker.addEventListener("install", (event: ExtendableEvent) => {
+    event.waitUntil(serviceWorker.skipWaiting())
+  })
+
+  browser.runtime.onMessage.addListener((message, sender) => {
+    if (message.type === "HEARTBEAT") {
+      return Promise.resolve({ processId: PROCESS_ID })
+    }
+  })
+
   // this supports both manifest v2 and v3
   const { startApi } = await import("@sendnodes/pokt-wallet-background")
   await startApi()
