cf21c469f0de371fa53f4aeec7be8eb78fa149a4
BigBoss <bigboss@sendnodes.io>
2022-12-19 22:10:10 -0600

    use custom eth and polygon rpc provider

--- packages/ui/hooks/validation-hooks.ts
+++ packages/ui/hooks/validation-hooks.ts
@@ -201,7 +201,23 @@ export const useAddressOrNameValidation: AsyncValidationHook<
       } else if (remoteConfig?.POKT?.features?.unstoppableDomains) {
         setIsValidating(true)
         validatingValue.current = trimmed
-        const resolution = new Resolution()
+        const resolution = new Resolution({
+          sourceConfig: {
+            uns: {
+              locations: {
+                Layer1: {
+                  url: process.env.ETH_MAINNET_RPC_URL,
+                  network: "mainnet",
+                },
+                Layer2: {
+                  url: process.env.POLYGON_MAINNET_RPC_URL,
+                  network: "polygon-mainnet",
+                },
+              },
+            },
+          },
+        })
+
         const resolved = await resolution
           .addr(trimmed, "POKT")
           .catch(() => undefined)
