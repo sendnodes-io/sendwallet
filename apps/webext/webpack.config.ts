ad68e91201e98afdae937d47fe026b7e29ad5b72
BigBoss <bigboss@sendnodes.io>
2022-12-26 13:04:33 -0600

    build sort of works

--- apps/webext/webpack.config.ts
+++ apps/webext/webpack.config.ts
@@ -41,6 +41,8 @@ const NODE_ENV = process.env.NODE_ENV || "development";
 const target = "web";
 const isProduction = NODE_ENV === "production";
 
+console.log("NODE_ENV", NODE_ENV);
+
 const uiRoot = path.resolve(__dirname, "..", "..", "packages", "ui-legacy");
 const outputDir = path.resolve(process.env.WEBPACK_OUTPUT_DIR || __dirname);
 
@@ -48,6 +50,8 @@ const tamaguiOptions: TamaguiOptions = {
 	config: "./tamagui.config.ts",
 	components: ["tamagui", "app", "@my/ui"],
 	importsWhitelist: [],
+	logTimings: true,
+	disableExtraction: !isProduction,
 };
 
 // Replicated and adjusted for each target browser and the current build mode.
@@ -76,9 +80,9 @@ const baseConfig: Configuration = {
 			{
 				oneOf: [
 					{
-						test: /.*\.[tj]sx?$/,
+						test: /.*\.[tj]sx$/,
 						use: [
-							// "thread-loader",
+							"thread-loader",
 							{
 								loader: "babel-loader",
 								options: {
@@ -92,6 +96,23 @@ const baseConfig: Configuration = {
 							{
 								loader: "tamagui-loader",
 								options: tamaguiOptions,
+							},
+						],
+					},
+
+					{
+						test: /.*\.[tj]s$/,
+						use: [
+							"thread-loader",
+							{
+								loader: "babel-loader",
+								options: {
+									cacheDirectory: true,
+									// The 'metro-react-native-babel-preset' preset is recommended to match React Native's packager
+									presets: ["module:metro-react-native-babel-preset"],
+									// Re-write paths to import only the modules needed by the app
+									plugins: ["react-native-web"],
+								},
 							},
 						],
 					},
