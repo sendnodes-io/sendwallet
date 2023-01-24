fd2d155f6cf959c529c72cd215ca00be80eba9c2
Big Boss <bigboss@sendnodes.io>
2023-01-24 11:51:42 -0600

    fix an issue with the service worker starting too slowly (#34)

--- apps/webext-legacy/src/background.ts
+++ apps/webext-legacy/src/background.ts
@@ -10,12 +10,6 @@
 		event.waitUntil(serviceWorker.skipWaiting());
 	});
 
-	browser.runtime.onMessage.addListener((message, sender) => {
-		if (message.type === "HEARTBEAT") {
-			return Promise.resolve({ processId: self.PROCESS_ID });
-		}
-	});
-
 	// this supports both manifest v2 and v3
 	const { startApi } = await import("@sendnodes/pokt-wallet-background");
 	await startApi();
---
+++ packages/ui-legacy/components/Core/CoreStartStore.tsx
@@ -0,0 +1,48 @@
+import { Store } from "@0xbigboss/webext-redux";
+import { browser, newProxyStore } from "@sendnodes/pokt-wallet-background";
+import { setActiveTab } from "@sendnodes/pokt-wallet-background/redux-slices/ui";
+import React, {
+	ComponentType,
+	CSSProperties,
+	useEffect,
+	useState,
+} from "react";
+import SharedSplashScreen from "../Shared/SharedSplashScreen";
+
+export default function CoreStartStore({
+	Component,
+}: { Component: ComponentType<{ store: Store }> }) {
+	const [store, setStore] = useState<Store | null>(null);
+
+	async function init() {
+		const backgroundStore = await newProxyStore();
+
+		// always set the active tab
+		const activeTab = await browser.tabs
+			.query({
+				active: true,
+				lastFocusedWindow: true,
+			})
+			.then((tabs) => (tabs[0] ? tabs[0] : null));
+		backgroundStore.dispatch(setActiveTab(activeTab));
+
+		setStore(backgroundStore);
+	}
+
+	useEffect(() => {
+		init();
+	}, []);
+
+	if (!store) {
+		return (
+			<div
+				className="flex flex-col items-center justify-center h-full dashed_border"
+				style={{ "--main-margin": 0 } as CSSProperties}
+			>
+				<SharedSplashScreen />
+			</div>
+		);
+	} else {
+		return <Component store={store} />;
+	}
+}
